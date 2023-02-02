be1884f6d86dc9e5bd7cffa1fc5e4f2380399d03
Milton Ly <ly.milton@gmail.com>
2023-02-01 17:15:28 -0800

    attempting to debug 'require' issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const inquirer = require('inquirer')
-const mysql = require('mysql')
-const cTable = require('console.table')
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+const cTable = require('console.table');
 const PORT = process.env.PORT || 3000
 
 const db = mysql.createConnection(
@@ -12,15 +12,29 @@ const db = mysql.createConnection(
     }
 )
 
-db.connect(function(err) {
+db.connect(function (err) {
     if (err) throw err
-    console.log (`connected to http://localhost:${PORT}`)
+    console.log(`connected to http://localhost:${PORT}`)
     startApp();
 })
 
 function startApp() {
     inquirer
-        prompt([
-
-        ])
+    .prompt([
+        {
+            message: "Welcome to the Employee Manager App"
+        },
+        {
+            type: 'list',
+            message: 'What Would you like to do?',
+            name: 'choice',
+            choices: ['View all Employees?',
+                      'View all Employees Jobs?', 
+                      'View all Employees by Department?', 
+                      'Update Employees', 'Add Employee', 
+                      'Add Job', 
+                      'Add Department'
+                     ]
+        }
+    ])
 }
